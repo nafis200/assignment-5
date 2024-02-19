c0187aacfc1ebd996d1343e641a314a2a6cb9dfa
nafis200 <nafisahamed14@gmail.com>
2024-02-18 23:02:14 -0500

    price add

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ let count = 1
 function Tickets(elementID){
      let num = getNumber('greenline')
      let num1 = getNumber('seat')
+     let price = getNumber('price')
      if(count == 4){
           alert('you cant buy more than 4 tickets')
      }
@@ -13,8 +14,12 @@ function Tickets(elementID){
         arr.push(elementID)
         let num2 = num - 1;
         let num3 = num1 + 1;
+        let num4 = price
+        let num5 = num4 + 550
+        console.log(num5)
         setelement('greenline',num2)
         setelement('seat',num3)
+        setelement('price',num5)
         count = count + 1;
         Setvalue('list')
      }
